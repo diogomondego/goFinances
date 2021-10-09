e9ef3a87a59bfebab1686a717e21fad63ad96384
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2021-10-09 17:25:50 -0300

    Cor do amount de acordo com o tipo de transação

--- src/Components/TransactionCard/index.tsx
+++ src/Components/TransactionCard/index.tsx
@@ -16,7 +16,8 @@ interface Category {
   icon: string;
 }
 
-interface Data {
+export interface TransactionCardProps {
+  type: 'positive' | 'negative'
   title: string;
   amount: string;
   category: Category;
@@ -24,7 +25,7 @@ interface Data {
 }
 
 interface Props {
-  data: Data
+  data: TransactionCardProps
 }
 
 export function TransactionCard ({ data } : Props) {
@@ -32,7 +33,10 @@ export function TransactionCard ({ data } : Props) {
     <Container>
       <Title>{data.title}</Title>
 
-      <Amount>{data.amount}</Amount>
+      <Amount type={data.type}>
+        {data.type === 'negative' && '- '}
+        {data.amount}
+      </Amount>
 
       <Footer>
         <Category>
